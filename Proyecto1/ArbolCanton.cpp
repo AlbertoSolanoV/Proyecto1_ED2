9b95df78a4f7bf6a55b07618a6523ee213a66baf
Alberto Solano <70608100+AlbertoSolanoV@users.noreply.github.com>
2023-07-05 16:12:14 -0600

    Cambios para leer el txt y buscar el canton

--- Proyecto1/ArbolCanton.cpp
+++ Proyecto1/ArbolCanton.cpp
@@ -2,117 +2,146 @@
 using namespace std;
 int ArbolCanton::obtenerAltura(NodoCanton* pNodoCanton)
 {
-    if (pNodoCanton == nullptr) {
-        return 0;
-    }
+	if (pNodoCanton == nullptr) {
+		return 0;
+	}
 
-    return pNodoCanton->getAltura();
+	return pNodoCanton->getAltura();
 }
 
 int ArbolCanton::obtenerFactorBalance(NodoCanton* pNodoCanton)
 {
-    if (pNodoCanton == nullptr) {
-        return 0;
-    }
-    return obtenerAltura(pNodoCanton->getIzquierda()) - obtenerAltura(pNodoCanton->getDerecha());
+	if (pNodoCanton == nullptr) {
+		return 0;
+	}
+	return obtenerAltura(pNodoCanton->getIzquierda()) - obtenerAltura(pNodoCanton->getDerecha());
 }
 
 NodoCanton* ArbolCanton::rotarDerecha(NodoCanton* y)
 {
-    NodoCanton* x = y->getIzquierda();
-    NodoCanton* T2 = x->getDerecha();
+	NodoCanton* x = y->getIzquierda();
+	NodoCanton* T2 = x->getDerecha();
 
-    x->setDerecha(y);
-    y->setIzquierda(T2);
+	x->setDerecha(y);
+	y->setIzquierda(T2);
 
-    y->setAltura(std::max(obtenerAltura(y->getIzquierda()), obtenerAltura(y->getDerecha())) + 1);
-    x->setAltura(std::max(obtenerAltura(x->getIzquierda()), obtenerAltura(x->getDerecha())) + 1);
+	y->setAltura(std::max(obtenerAltura(y->getIzquierda()), obtenerAltura(y->getDerecha())) + 1);
+	x->setAltura(std::max(obtenerAltura(x->getIzquierda()), obtenerAltura(x->getDerecha())) + 1);
 
-    return x;
+	return x;
 }
 
 NodoCanton* ArbolCanton::rotarIzquierda(NodoCanton* x)
 {
-    NodoCanton* y = x->getDerecha();
-    NodoCanton* T2 = y->getIzquierda();
+	NodoCanton* y = x->getDerecha();
+	NodoCanton* T2 = y->getIzquierda();
 
-    y->setIzquierda(x);
-    x->setDerecha(T2);
+	y->setIzquierda(x);
+	x->setDerecha(T2);
 
-    x->setAltura(std::max(obtenerAltura(x->getIzquierda()), obtenerAltura(x->getDerecha())) + 1);
-    y->setAltura(std::max(obtenerAltura(y->getIzquierda()), obtenerAltura(y->getDerecha())) + 1);
+	x->setAltura(std::max(obtenerAltura(x->getIzquierda()), obtenerAltura(x->getDerecha())) + 1);
+	y->setAltura(std::max(obtenerAltura(y->getIzquierda()), obtenerAltura(y->getDerecha())) + 1);
 
-    return y;
+	return y;
 }
 
 NodoCanton* ArbolCanton::insertar(NodoCanton* pNodoCanton, Canton pCanton)
 {
-    if (pNodoCanton == nullptr) {
-        return new NodoCanton(pCanton);
-    }
+	if (pNodoCanton == nullptr) {
+		return new NodoCanton(pCanton);
+	}
 
-    if (pCanton.getCabecera() < pNodoCanton->getCanton().getCabecera()) {
-        pNodoCanton->setIzquierda(insertar(pNodoCanton->getIzquierda(), pCanton));
-    }
-    else if (pCanton.getCabecera() > pNodoCanton->getCanton().getCabecera()) {
-        pNodoCanton->setDerecha(insertar(pNodoCanton->getDerecha(), pCanton));
-    }
-    else {
-        // No se permiten claves duplicadas en árboles AVL
-        return pNodoCanton;
-    }
+	if (pCanton.getCabecera() < pNodoCanton->getCanton().getCabecera()) {
+		pNodoCanton->setIzquierda(insertar(pNodoCanton->getIzquierda(), pCanton));
+	}
+	else if (pCanton.getCabecera() > pNodoCanton->getCanton().getCabecera()) {
+		pNodoCanton->setDerecha(insertar(pNodoCanton->getDerecha(), pCanton));
+	}
+	else {
+		// No se permiten claves duplicadas en árboles AVL
+		return pNodoCanton;
+	}
 
-    pNodoCanton->setAltura(1 + std::max(obtenerAltura(pNodoCanton->getIzquierda()), obtenerAltura(pNodoCanton->getDerecha())));
+	pNodoCanton->setAltura(1 + std::max(obtenerAltura(pNodoCanton->getIzquierda()), obtenerAltura(pNodoCanton->getDerecha())));
 
-    int factorBalance = obtenerFactorBalance(pNodoCanton);
+	int factorBalance = obtenerFactorBalance(pNodoCanton);
 
-    // Caso Izquierda - Izquierda
-    if (factorBalance > 1 && pCanton.getCabecera() < pNodoCanton->getIzquierda()->getCanton().getCabecera()) {
-        return rotarDerecha(pNodoCanton);
-    }
+	// Caso Izquierda - Izquierda
+	if (factorBalance > 1 && pCanton.getCabecera() < pNodoCanton->getIzquierda()->getCanton().getCabecera()) {
+		return rotarDerecha(pNodoCanton);
+	}
 
-    // Caso Derecha - Derecha
-    if (factorBalance < -1 && pCanton.getCabecera() > pNodoCanton->getDerecha()->getCanton().getCabecera()) {
-        return rotarIzquierda(pNodoCanton);
-    }
+	// Caso Derecha - Derecha
+	if (factorBalance < -1 && pCanton.getCabecera() > pNodoCanton->getDerecha()->getCanton().getCabecera()) {
+		return rotarIzquierda(pNodoCanton);
+	}
 
-    // Caso Izquierda - Derecha
-    if (factorBalance > 1 && pCanton.getCabecera() > pNodoCanton->getIzquierda()->getCanton().getCabecera()) {
-        pNodoCanton->setIzquierda(rotarIzquierda(pNodoCanton->getIzquierda()));
-        return rotarDerecha(pNodoCanton);
-    }
+	// Caso Izquierda - Derecha
+	if (factorBalance > 1 && pCanton.getCabecera() > pNodoCanton->getIzquierda()->getCanton().getCabecera()) {
+		pNodoCanton->setIzquierda(rotarIzquierda(pNodoCanton->getIzquierda()));
+		return rotarDerecha(pNodoCanton);
+	}
 
-    // Caso Derecha - Izquierda
-    if (factorBalance < -1 && pCanton.getCabecera() < pNodoCanton->getDerecha()->getCanton().getCabecera()) {
-        pNodoCanton->setDerecha(rotarDerecha(pNodoCanton->getDerecha()));
-        return rotarIzquierda(pNodoCanton);
-    }
+	// Caso Derecha - Izquierda
+	if (factorBalance < -1 && pCanton.getCabecera() < pNodoCanton->getDerecha()->getCanton().getCabecera()) {
+		pNodoCanton->setDerecha(rotarDerecha(pNodoCanton->getDerecha()));
+		return rotarIzquierda(pNodoCanton);
+	}
 
-    return pNodoCanton;
+	return pNodoCanton;
 }
 
 void ArbolCanton::mostrar(NodoCanton* pNodoCanton)
 {
-    if (pNodoCanton == nullptr) {
-        return;
-    }
+	if (pNodoCanton == nullptr) {
+		return;
+	}
 
-    mostrar(pNodoCanton->getIzquierda());
-    cout << pNodoCanton->getCanton().getNombre() << " ";
-    mostrar(pNodoCanton->getDerecha());
+	mostrar(pNodoCanton->getIzquierda());
+	cout << pNodoCanton->getCanton().getNombre() << " - ";
+	mostrar(pNodoCanton->getDerecha());
 }
 
 ArbolCanton::ArbolCanton()
 {
-        raiz = nullptr;
+	raiz = nullptr;
 }
 
 void ArbolCanton::insertar(Canton pCanton)
 {
-    raiz = insertar(raiz, pCanton);
+	raiz = insertar(raiz, pCanton);
 }
 
 void ArbolCanton::mostrar()
 {
-    mostrar(raiz);
+	mostrar(raiz);
+}
+
+void ArbolCanton::buscarPorNombre(string pNombreCanton)
+{
+	buscar(pNombreCanton, raiz);
+}
+
+NodoCanton* ArbolCanton::buscar(string pNombreCanton, NodoCanton* pNodoCanton)
+{
+
+	if (pNodoCanton == nullptr)
+		return pNodoCanton;
+
+	if (pNodoCanton->getCanton().getNombre() == pNombreCanton) {
+		cout << "Informacion del canton:" << endl;
+		cout << "Cabecera: " << pNodoCanton->getCanton().getCabecera() << endl;
+		cout.flush();
+		cout << "Cantidad de habitantes: " << pNodoCanton->getCanton().getCantidadPersona() << endl;
+		cout.flush();
+		cout << "Nombre del alcalde: " << pNodoCanton->getCanton().getNombreAlcalde() << endl;
+		return pNodoCanton;
+	}
+		
+
+	if (pNombreCanton < pNodoCanton->getCanton().getNombre())
+		return buscar(pNombreCanton, pNodoCanton->getIzquierda());
+	else
+		return buscar(pNombreCanton, pNodoCanton->getDerecha());
+
 }
